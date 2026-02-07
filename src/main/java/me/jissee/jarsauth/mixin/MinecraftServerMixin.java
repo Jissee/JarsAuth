e5f8de43a72d89f267cf2671e4ff3709c4ad60a8
Jissee <2431740933@qq.com>
2026-02-06 16:42:15 -0800

    add mixin test

--- src/main/java/me/jissee/jarsauth/mixin/MinecraftServerMixin.java
+++ src/main/java/me/jissee/jarsauth/mixin/MinecraftServerMixin.java
@@ -7,6 +7,8 @@ import me.jissee.jarsauth.wrap.Assert;
 import net.minecraft.server.MinecraftServer;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.fml.loading.FMLLoader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -14,8 +16,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftServer.class)
 public class MinecraftServerMixin {
+    private static final Logger LOGGER = LoggerFactory.getLogger("JarsAuth Mixin");
+
     @Inject(method = "runServer", at = @At("HEAD"))
     private void onRunServer(CallbackInfo ci){
+        LOGGER.info("Mixin applied successfully");
         if(Assert.assertFalse(true)) return;
         if(FMLLoader.getDist() == Dist.DEDICATED_SERVER){
             AbstractPendingList.init((MinecraftServer) (Object) this);
